0e3277e5545cb54b26fec5fe64ea9891b7ce5149
003HRZ744 <ravivarmak09612@gmail.com>
2023-01-15 16:49:58 +0530

    version 2

--- src/main/java/com/example/demomongo/noobcoderravivarma/Exceptions/UserNotFoundException.java
+++ src/main/java/com/example/demomongo/noobcoderravivarma/Exceptions/UserNotFoundException.java
@@ -1,2 +1,13 @@
-package com.example.demomongo.noobcoderravivarma.Exceptions;public class UserNotFoundException {
+package com.example.demomongo.noobcoderravivarma.Exceptions;
+
+import org.apache.logging.log4j.message.Message;
+
+import java.util.function.Supplier;
+
+public class UserNotFoundException extends Exception  {
+    public UserNotFoundException(String message) {
+        super(message);
+        System.out.println(message.toUpperCase());
+    }
+
 }
